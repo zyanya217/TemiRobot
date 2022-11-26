675f753430eb561032557f4e0a8cb16d6fc24be5
qinelin <qinerinsky@gmail.com>
2022-11-27 03:48:11 +0800

    update log function function name -> test() author : elin

--- app/src/main/java/tw/com/temirobot/Todo.java
+++ app/src/main/java/tw/com/temirobot/Todo.java
@@ -14,8 +14,6 @@ public class Todo extends AppCompatActivity {
     private Intent it2;
     private TextView detectionTextView;
 
-    private final static int sendUser = 2;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,26 +37,8 @@ public class Todo extends AppCompatActivity {
 
     public void btnyes(View v){
         Intent it = new Intent(Todo.this,Todolist.class);
-        it2 = getIntent();
-        // 通過key得到得到物件
-        // getSerializableExtra得到序列化資料
-        String name = (String) it2.getSerializableExtra("key");
-        // 放入需要傳遞的物件
-        it.putExtra("key", name);
-        // 啟動意圖(意圖，請求碼(int)) 請求碼最好使用 final static定義 方便識別
-        startActivityForResult(it, sendUser);
         startActivity(it);
         finish();
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        // 如果請求碼為 sendUser 返回碼 為 RESULT_OK RESULT_OK為系統自定義的int值為 -1
-        if (requestCode == sendUser && resultCode == RESULT_OK) {
-            // 在TextView中設定返回資訊
-            System.out.println("list: 傳遞訊息: "+data.getStringExtra("key"));
-        }
     }
 
     public void btnno(View v){
