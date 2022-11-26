0853e87693e4f1b03d38d3e838670c2ab00de1b5
qinelin <qinerinsky@gmail.com>
2022-11-27 01:58:23 +0800

    update log function function name -> test() author : elin

---
+++ app/src/main/java/tw/com/temirobot/EquipmenTeaching.java
@@ -0,0 +1,14 @@
+package tw.com.temirobot;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EquipmenTeaching extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_equipmen_teaching);
+    }
+}
