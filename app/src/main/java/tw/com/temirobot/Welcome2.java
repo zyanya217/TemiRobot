45ac9c639b0e852f8dcf3b0228cdd19f3b114bf1
qinelin <qinerinsky@gmail.com>
2022-12-02 01:53:24 +0800

    update log function function name -> test() author : elin

---
+++ app/src/main/java/tw/com/temirobot/Welcome2.java
@@ -0,0 +1,14 @@
+package tw.com.temirobot;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Welcome2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome2);
+    }
+}
