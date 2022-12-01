561fb8665d30e3f434a052aa04c1866a75d1438f
qinelin <qinerinsky@gmail.com>
2022-12-01 21:07:31 +0800

    update log function function name -> test() author : elin

---
+++ app/src/main/java/tw/com/temirobot/FaceRecognition2.java
@@ -0,0 +1,14 @@
+package tw.com.temirobot;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FaceRecognition2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_face_recognition2);
+    }
+}
